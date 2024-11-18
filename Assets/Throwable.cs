fcc13f3b2bab722ab224876c74c9fed687f425ee
Evan Murray <evanesce@email.unc.edu>
2024-11-17 20:32:52 -0500

    Fix dart collision

--- Assets/Throwable.cs
+++ Assets/Throwable.cs
@@ -5,41 +5,44 @@ using UnityEngine;
 public class Throwable : MonoBehaviour
 {
     Rigidbody rb;
+    OVRGrabbable grabbable;
     bool targetHit = false;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-     
+        rb = GetComponent<Rigidbody>();
+        grabbable = GetComponent<OVRGrabbable>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (targetHit)
+        if (collision.gameObject.CompareTag("Board"))
         {
-            return;
-        }
-        else
-        {
-            targetHit = true;
-        }
+            Debug.Log("colided");
 
-        rb.isKinematic = true; // makes projectile stick to surface
+            if (targetHit)
+            {
+                return;
+            }
+            else
+            {
+                targetHit = true;
+            }
 
-        transform.SetParent(collision.transform);
+            rb.isKinematic = true; // makes projectile stick to surface
 
-        // Check if velocity of dart is high enough to stick (test magnitude value)
-        /*if(collision.relativeVelocity.magnitude > 1)
-        {
-            rb.isKinematic = true;
             transform.SetParent(collision.transform);
-        }*/
+
+            Destroy(grabbable);
+
+            // Check if velocity of dart is high enough to stick (test magnitude value)
+            /*if(collision.relativeVelocity.magnitude > 1)
+            {
+                rb.isKinematic = true;
+                transform.SetParent(collision.transform);
+            }*/
+        }
     }
 }
